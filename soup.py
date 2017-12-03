8180aa6d1eecf7c32733309f670804f90380aa73
Sanskar Agrawal <sanskaragrawal.coolguy@gmail.com>
2017-12-04 03:26:30 +0530

    Added findChildrens function

--- soup.py
+++ soup.py
@@ -1,9 +1,19 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-url="http://www.cprogramming.com/snippets/source-code/templated-stack-class"
+url="https://github.com/sanskar-sopho/Cat-Dog_classifier/blob/master/cat_dog.py"
 page=urllib2.urlopen(url)
 
-soup=BeautifulSoup(page,'html.parser')
-table=soup.find_all('table',class_='main')
-for 
+soup=BeautifulSoup(page,'html.parser')	
+# repcontent=(soup.find(class_="repository-content")).find_all('table')
+# for row in soup.find(class_="repository-content").find_all("tr"):
+# 	print(row,'\n')
+
+tables=soup.findChildren('table')
+# print(tables)
+my_table=tables[0]
+rows=my_table.findChildren(['th','tr'])
+for row in rows:
+	cells=row.findChildren('td')
+	span=cells[1].findChildren('span')
+	
