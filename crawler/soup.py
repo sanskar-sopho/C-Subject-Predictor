c611fc557714e3b3e8e7431dacbd65b3f5153ef1
Sanskar Agrawal <sanskaragrawal.coolguy@gmail.com>
2017-12-05 14:48:00 +0530

    Working with line changing problem

--- crawler/soup.py
+++ crawler/soup.py
@@ -4,7 +4,7 @@ import urllib2
 url="https://github.com/sanskar-sopho/Cat-Dog_classifier/blob/master/cat_dog.py"
 page=urllib2.urlopen(url)
 
-soup=BeautifulSoup(page,'html.parser')	
+soup=BeautifulSoup(page,'xml')	
 # repcontent=(soup.find(class_="repository-content")).find_all('table')
 # for row in soup.find(class_="repository-content").find_all("tr"):
 # 	print(row,'\n')
@@ -15,5 +15,11 @@ my_table=tables[0]
 rows=my_table.findChildren(['th','tr'])
 for row in rows:
 	cells=row.findChildren('td')
-	span=cells[1].findChildren('span')
-	print(span)
+	for cell in cells:		
+		for string in cell.stripped_strings:
+			string=str(unicode(string))
+			print(repr(string))
+
+		# spans=cells[1].findChildren('span')
+		# for span in spans:
+			# print(span.string)
