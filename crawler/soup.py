7c053efc7bd776e8f73db5035214d39f8aa82ac4
Sanskar Agrawal <sanskaragrawal.coolguy@gmail.com>
2017-12-11 02:02:08 +0530

    Added recursion

--- crawler/soup.py
+++ crawler/soup.py
@@ -1,13 +1,18 @@
 from bs4 import BeautifulSoup
 import urllib2
+import sys
 
-url="https://github.com/sanskar-sopho/road-segmentation/"
+repo="https://github.com/sanskar-sopho/road-segmentation/"
+base_link='http://github.com'
+file_count=0
 
 def extract_code(url,file_name):
+	if(file_count>=50):
+		sys.exit
 	page=urllib2.urlopen(url)
 	soup=BeautifulSoup(page,'xml')
 	file=open(file_name,'w')
-	
+	file_count+=1
 	# repcontent=(soup.find(class_="repository-content")).find_all('table')
 	# for row in soup.find(class_="repository-content").find_all("tr"):
 	# 	print(row,'\n')
@@ -34,16 +39,27 @@ def extract_code(url,file_name):
 		file.write('\n')
 	return datetime
 
-
-page=urllib2.urlopen(url)
-soup=BeautifulSoup(page,'xml')
-table=soup.find('table')
-rows=table.find_all(['th','tr'])
-for row in rows:
-	if('js-navigation-item' not in row['class']):
-		continue
-	cols=row.find_all('td')
-	for col in cols:
-		if('content' not in col['class']):
+def expand_folder(url)
+	print "folder : ", url
+	page=urllib2.urlopen(url)
+	soup=BeautifulSoup(page,'xml')
+	table=soup.find('table')
+	rows=table.find_all(['th','tr'])
+	for row in rows:
+		if('js-navigation-item' not in row['class']):
 			continue
-		print col
+		cols=row.find_all('td')
+		for col in cols:
+			if('content' not in col['class']):
+				continue
+			tag=col.find('a')
+			link=tag['href']
+			if('tree' in link):
+				expand_folder(base_link+link)
+			if('blob' in link):
+				datetime=extract_code(base_link+link,str(file_count)+'.txt')
+				print('file written ', datetime)
+			else:
+				return
+
+	expand_folder(repo)
