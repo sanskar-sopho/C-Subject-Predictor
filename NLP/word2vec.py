d1884e202170c59167c77eb5f6f1a6f060bdfbdc
Sanskar Agrawal <sanskaragrawal.coolguy@gmail.com>
2017-12-19 03:01:13 +0530

    Prepared Training data

--- NLP/word2vec.py
+++ NLP/word2vec.py
@@ -11,8 +11,10 @@ cursor = db.cursor()
 sql = 'Select COMMENT_TEXT from AllComments';
 cursor.execute(sql)
 AllWords = []
+sentences = []
 for row in cursor:
     comment = row[0];
+    sentences.append(comment)
     words = comment.split()
     # print(words)
     for w in words :	
@@ -28,4 +30,18 @@ for i,word in enumerate(Unique_Words):
 	word2int[word]=i
 	int2word[i]=word
 
+# print(sentences[78])
+train_data=[]
+WINDOW_SIZE=1
+for sentence in sentences:
+	words=sentence.split()
+	for index in range(0,len(words)):
+		for i in range(-1*WINDOW_SIZE,WINDOW_SIZE+1,1):
+			if(index+i<0 or i==0 or index+i>=len(words)):
+				continue
+			train_data.append([words[index],words[index+i]])
+	
+# print train_data
+# print(len(train_data))
+# print(sentences[len(sentences)-3])
 
