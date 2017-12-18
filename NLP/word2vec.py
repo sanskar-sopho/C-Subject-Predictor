067cfe05cbcaa22d30f136cd2a71e2391f1c4deb
Sanskar Agrawal <sanskaragrawal.coolguy@gmail.com>
2017-12-19 00:49:07 +0530

    word2vec first commit

--- NLP/word2vec.py
+++ NLP/word2vec.py
@@ -1,6 +1,30 @@
-from gensim.models import Word2Vec
-from nltk.corpus import brown
+import numpy as np
+import tensorflow as tf
+import MySQLdb
 
-model=Word2Vec(brown.sents())
-print(model.most_similar('money',topn=5))
+# Open database connection
+db = MySQLdb.connect("localhost","root","1698809","Comments" )
 
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+sql = 'Select COMMENT_TEXT from AllComments';
+cursor.execute(sql)
+AllWords = []
+for row in cursor:
+    comment = row[0];
+    words = comment.split()
+    print(words)
+    for w in words :	
+        AllWords.append(w)
+# print('-----------List of All the Words----------------')
+# print(FinalStrWords)
+# print('------------Count of All words------------------')
+# print(len(FinalStrWords))
+
+Unique_Words = list(set(AllWords))
+# print('------------List of Unique Words----------------')
+print(Unique_Words)
+# print('------------Count of Unique Words---------------')
+# print(len(Unique_Word_List))
+db.close()
