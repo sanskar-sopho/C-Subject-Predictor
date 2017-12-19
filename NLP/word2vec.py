c7b274cab53c35447d41ea03c037e6127d20ae4a
Sanskar Agrawal <sanskaragrawal.coolguy@gmail.com>
2017-12-19 06:26:53 +0530

    Working word2vec

--- NLP/word2vec.py
+++ NLP/word2vec.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import MySQLdb
+import math
 
 # Open database connection
 db = MySQLdb.connect("localhost","root","1698809","Comments" )
@@ -45,3 +46,62 @@ for sentence in sentences:
 # print(len(train_data))
 # print(sentences[len(sentences)-3])
 
+train_x=[]
+train_y=[]
+for data in train_data:
+	train_x.append(word2int[data[0]])
+	train_y.append(word2int[data[1]])
+train_x=np.array(train_x)
+train_y=np.array(train_y)
+train_x=np.reshape(train_x,[-1,1])
+train_y=np.reshape(train_y,[-1,1])
+print(train_x.shape,train_y.shape)
+
+vocab_size=len(Unique_Words)
+embed_size=100
+num_sampled=30
+num_iter=100000
+
+X=tf.placeholder(tf.int32,shape=[None,1])
+Y=tf.placeholder(tf.int32,shape=[None,1])
+
+embeddings=tf.Variable(tf.random_uniform([vocab_size,embed_size],-1.0,1.0),dtype=tf.float32)
+soft_weights=tf.Variable(tf.truncated_normal([vocab_size,embed_size],stddev=1.0/math.sqrt(embed_size)),dtype=tf.float32)
+soft_biases=tf.Variable(tf.zeros([vocab_size]),dtype=tf.float32)
+# print soft_weights,soft_biases
+
+# temp=tf.Variable(tf.zeros([None,embed_size]))
+embed=tf.nn.embedding_lookup(embeddings,X)
+embed=tf.reduce_sum(embed,1)
+print embed
+print Y
+loss=tf.reduce_mean(tf.nn.sampled_softmax_loss(soft_weights,soft_biases,Y,embed,num_sampled,vocab_size))
+
+optimizer=tf.train.AdagradOptimizer(0.1).minimize(loss)
+
+def train():
+	saver=tf.train.Saver([embeddings,soft_weights,soft_biases])
+	for i in range(0,num_iter):
+		feed_dict={X:train_x,Y:train_y}
+		_,l=sess.run([optimizer,loss],feed_dict)
+		if(i%100==0):
+			print i," loss = ",l
+		if(i%10000==0 and i != 0):
+			save_path=saver.save(sess,'restore/model.ckpt')
+			print "Successfully saved in ",save_path	
+	save_path=saver.save(sess,'restore/model.ckpt')
+	print "Successfully saved in ",save_path
+
+def restore():
+	saver=tf.train.Saver([embeddings,soft_weights,soft_biases])
+	# tf.reset_default_graph()
+	saver.restore(sess,"restore/model.ckpt")
+	print "Successfully Restored"
+
+
+with tf.Session() as sess:
+	tf.global_variables_initializer().run()
+	# train()
+	# restore()
+	# print(sess.run(loss,feed_dict={X:train_x,Y:train_y}))
+	# print(sess.run(embeddings))
