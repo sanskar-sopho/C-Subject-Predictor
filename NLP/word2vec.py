8209b2edc98394cfda94a4a5ff40f528819520c2
Sanskar Agrawal <sanskaragrawal.coolguy@gmail.com>
2017-12-20 21:27:53 +0530

    RNN Trainable

--- NLP/word2vec.py
+++ NLP/word2vec.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import MySQLdb
 import math
+from tensorflow.contrib.layers import fully_connected
 
 # Open database connection
 db = MySQLdb.connect("localhost","root","1698809","Comments" )
@@ -60,7 +61,7 @@ print(train_x.shape,train_y.shape)
 vocab_size=len(Unique_Words)
 embed_size=100
 num_sampled=30
-num_iter=100000
+num_iter=10000
 
 X=tf.placeholder(tf.int32,shape=[None,1])
 Y=tf.placeholder(tf.int32,shape=[None,1])
@@ -105,16 +106,45 @@ with tf.Session() as sess:
 	restore()
 	# print(sess.run(loss,feed_dict={X:train_x,Y:train_y}))
 	embeddings=np.array(sess.run(embeddings))
-	print embeddings
+	print embeddings[3]
+
+
 
 # We have word2vec, now defining RNN
+# 0 Memory 1 Operation 2 Algorithm 3 Concurrency
+data=['assign','memory','contiguous','allocated','storage','file handl','sub problem','iterative sort','bucket sort','insertion sort','merge sort','replace','puts','gets','declaration','initialisation','Step','insert','select','insert','add','delete','deadlock','livelock','synchronisation','data race']
+labels=[0,0,0,0,0,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,3,3,3,3]
 
-n_steps=5
+
+n_steps=3
 n_feature=embed_size
 n_neurons=45
-n_class=3
+n_class=4
 l_rate=0.001
+num_iter=1000
 
+X_=[]
+for element in data:
+	words=element.split()
+	i=0
+	for word in words:
+		X_.append(embeddings[word2int[word]])
+		i+=1
+	while(i<n_steps):
+		X_.append([0 for j in range(0,n_feature)])
+		i+=1
+
+X_=np.array(X_)
+Y_=[]
+for label in labels:
+	Y_.append([(1 if i==label else 0) for i in range(0,n_class)])
+Y_=np.array(Y_)
+print(X_.shape,Y_.shape)
+
+X_=X_.reshape((-1,n_steps,n_feature))
+Y_=Y_.reshape((-1,n_class))
+
+print X_.shape,Y_.shape
 tf.reset_default_graph()
 
 X=tf.placeholder(tf.float32,[None,n_steps,n_feature])
@@ -126,12 +156,33 @@ outputs,states=tf.nn.dynamic_rnn(basic_cell,X,dtype=tf.float32)
 
 outputs=tf.unstack(tf.transpose(outputs,perm=[1,0,2]))
 last_output=outputs[-1]
+print last_output
 
 logits=fully_connected(last_output,n_class,activation_fn=None)
-cross_entropy=tf.nn.softmax_cross_entropy_with_logits=(logits,Y)
+cross_entropy=tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=Y)
 
 loss=tf.reduce_mean(cross_entropy)
 train_step=tf.train.AdamOptimizer(l_rate).minimize(loss)
 
 correct=tf.equal(tf.argmax(logits,1),tf.argmax(Y,1))
 accuracy=tf.reduce_mean(tf.cast(correct,tf.float32))
+
+def train_rnn():
+	saver=tf.train.Saver()
+	for i in range(0,num_iter):
+		sess.run(train_step,feed_dict={X:X_,Y:Y_})
+		if(i%50==0):
+			print "loss is ",loss.eval(feed_dict={X:X_,Y:Y_})
+	save_path=saver.save(sess,'rnn_saver/model.ckpt')
+	print "Successfully saved in ",save_path
+
+def restore_rnn():
+	saver=tf.train.Saver()
+	saver.restore('rnn_saver/model.ckpt')
+	print "Successfully restored"
+
+with tf.Session() as sess:
+	tf.global_variables_initializer().run()
+	# train_rnn()
+	restore_rnn()
+	print "Accuracy = ",accuracy.eval(feed_dict={X:X_,Y:Y_})
